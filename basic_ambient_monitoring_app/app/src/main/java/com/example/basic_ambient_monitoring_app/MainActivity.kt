1f130daee8edc1c597f331638fe24f2456e8effe
droktorX <j.sachwitz@gmx.de>
2022-10-23 19:21:43 +0200

    add light sensor to ambient monitoring app

--- basic_ambient_monitoring_app/app/src/main/java/com/example/basic_ambient_monitoring_app/MainActivity.kt
+++ basic_ambient_monitoring_app/app/src/main/java/com/example/basic_ambient_monitoring_app/MainActivity.kt
@@ -17,7 +17,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener{
 
     private lateinit var sensorManager: SensorManager
     private var ambientTemperature: Sensor? = null
-    var temp: String = ""
+    private var lightSensor: Sensor? = null
+    var temp: String = "\n no data"
+    var light: String = "\n no data"
     val scrollView: ScrollView by lazy { findViewById<ScrollView>(R.id.scrollview) }
 
 
@@ -25,6 +27,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener{
         if (event != null) {
             if (event.sensor.type == Sensor.TYPE_AMBIENT_TEMPERATURE) {
                 temp = "\n" + event.values[0].toString()
+            }
+            if (event.sensor.type == Sensor.TYPE_LIGHT) {
+                light = "\n" + event.values[0].toString()
             }
         }
     }
@@ -42,18 +47,24 @@ class MainActivity : AppCompatActivity(), SensorEventListener{
         // a particular sensor.
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager;
         ambientTemperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         if (ambientTemperature == null) {
-            findViewById<TextView>(R.id.textView_temperatur).text = "No ambient temperature sensor found."
+            findViewById<TextView>(R.id.textView_temperatur).text = "No temperature sensor found."
+        }
+        if (lightSensor == null) {
+            findViewById<TextView>(R.id.textView_light).text = "No light sensor found."
         }
 
-        val textview: TextView = findViewById(R.id.textView_temperatur)
+        val textview_temperature: TextView = findViewById(R.id.textView_temperatur)
+        val textview_light: TextView = findViewById(R.id.textView_light)
 
         val handler = Handler(Looper.getMainLooper())
         handler.post(object : Runnable {
             override fun run() {
-                textview.append(temp)
+                textview_temperature.append(temp)
+                textview_light.append(light)
                 scrollView.fullScroll(ScrollView.FOCUS_DOWN)
-                handler.postDelayed(this, 100);
+                handler.postDelayed(this, 250);
             }
         })
 
@@ -62,6 +73,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener{
     override fun onResume() {
         super.onResume()
         sensorManager.registerListener(this, ambientTemperature, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
